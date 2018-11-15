927d740621216751a5da1b89ec44b981c97d1e89
Franklin Willemen <franklinwillemen@protonmail.com>
2018-11-15 13:49:26 +0100

    Create Separating_populations_with_Boolean_indexing.py

---
+++ pandas-foundations/Visual exploratory data analysis/Separating_populations_with_Boolean_indexing.py
@@ -0,0 +1,29 @@
+# Compute the global mean and global standard deviation: global_mean, global_std
+global_mean = df.mean()
+global_std = df.std()
+
+# Filter the US population from the origin column: us
+us = df[df["origin"] == "US"]
+
+# Compute the US mean and US standard deviation: us_mean, us_std
+us_mean = us.mean()
+us_std = us.std()
+
+# Print the differences
+print(us_mean - global_mean)
+print(us_std - global_std)
+
+# Display the box plots on 3 separate rows and 1 column
+fig, axes = plt.subplots(nrows=3, ncols=1)
+
+# Generate a box plot of the fare prices for the First passenger class
+titanic.loc[titanic['pclass'] == 1].plot(ax=axes[0], y='fare', kind='box')
+
+# Generate a box plot of the fare prices for the Second passenger class
+titanic.loc[titanic['pclass'] == 2].plot(ax=axes[1], y='fare', kind='box')
+
+# Generate a box plot of the fare prices for the Third passenger class
+titanic.loc[titanic['pclass'] == 3].plot(ax=axes[2], y='fare', kind='box')
+
+# Display the plot
+plt.show()
