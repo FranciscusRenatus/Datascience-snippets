3e7b6c525287e95a88a6e71f40132bfb2cd04d04
Franklin Willemen <franklinwillemen@protonmail.com>
2018-11-15 17:38:10 +0100

    Create Visualizing_pandas_time_series.py

---
+++ pandas-foundations/time_series_in_pandas/Visualizing_pandas_time_series.py
@@ -0,0 +1,25 @@
+# Plot the raw data before setting the datetime index
+df.plot()
+plt.show()
+
+# Convert the 'Date' column into a collection of datetime objects: df.Date
+df.Date = pd.to_datetime(df.Date)
+
+# Set the index to be the converted 'Date' column
+df.set_index("Date",inplace = True)
+
+# Re-plot the DataFrame to see that the axis is now datetime aware!
+df.plot()
+plt.show()
+
+
+# Plot the summer data
+df.Temperature['2010-Jun':'2010-Aug'].plot()
+plt.show()
+plt.clf()
+
+# Plot the one week data
+df.Temperature['2010-06-10':'2010-06-17'].plot()
+plt.show()
+plt.clf()
+
